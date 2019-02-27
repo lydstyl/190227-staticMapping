19f9af97b98d79a1522009fe9191ee104f0e05a4
Gabriel BRUN <lydstyl@gmail.com>
2019-02-27 18:08:34 +0100

    script tested it is ok

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// <legacy URL> [i] p,[<protocol>],[<host>],<pipeline>[,<locale>][,<parameter name>,<parameter value>]*
-//     Search-ShowContent          /produits/pour-lui p,https,,Search-ShowContent,,fdid,Conseils-homme
-//     Search-Show                 /pour-elle/coiffure/seche-cheveux-pour-elle/  p,https,,Search-Show,,cgid,seche-cheveux
-//     Product-Show                /miroir-lumineux-rond-double-face  p,https,,Product-Show,,pid,8438E
-//     CustomerService-ContactUs   /assistance/  p,https,,CustomerService-ContactUs
-//     Home-Show                   /bandes-de-cire-froide-pour-jambes-corps-peaux-normales  p,https,,Home-Show
-//     Stores-Find                 /points-de-vente/ p,https,,Stores-Find
+/* Give to this script the appropriate .csv and it will generate the static mappings 
+
+    <legacy URL> [i] p,[<protocol>],[<host>],<pipeline>[,<locale>][,<parameter name>,<parameter value>]*
+    
+    Search-ShowContent          /produits/pour-lui p,https,,Search-ShowContent,,fdid,Conseils-homme
+    Search-Show                 /pour-elle/coiffure/seche-cheveux-pour-elle/  p,https,,Search-Show,,cgid,seche-cheveux
+    Product-Show                /miroir-lumineux-rond-double-face  p,https,,Product-Show,,pid,8438E
+    CustomerService-ContactUs   /assistance/  p,https,,CustomerService-ContactUs
+    Home-Show                   /bandes-de-cire-froide-pour-jambes-corps-peaux-normales  p,https,,Home-Show
+    Stores-Find                 /points-de-vente/ p,https,,Stores-Find
+
+*/
 
 // read csv
 const fs = require('fs')
 const csv = require('csvtojson')
-const csvFilePath='./csv2.csv'
-// Async / await usage
-// const jsonArray=await csv().fromFile(csvFilePath);
+const csvFilePath='./csv.csv'
 const types = {
     'CustomerService-ContactUs': 'p,https,,CustomerService-ContactUs',
     'Home-Show': 'p,https,,Home-Show',
@@ -22,7 +25,6 @@ const types = {
 }
 
 const leftTestUrl = 'https://staging-na-conair.demandware.net/s/es-babyliss' // + /produits/pour-lui
-let staticMappings = ''
 let testUrls = ''
 csv()
 .fromFile(csvFilePath)
@@ -34,9 +36,8 @@ csv()
         if ( right.includes( '!!ID!!' ) ) {
             right = right.replace('!!ID!!', line.ID)
         }
-        staticMappings += `${left} ${right}\n`
         testUrls += `<div><a target="_blank" href="${leftTestUrl}${left}">${left} ${right}</a></div>\n`
     })
-    fs.writeFileSync('./staticMappings.txt', staticMappings, 'utf8')
     fs.writeFileSync('./testUrls.html', testUrls, 'utf8')
+    fs.writeFileSync('./testUrls.html.txt', testUrls, 'utf8') // for uploading the file in Redmine that doesn't accept .html
 })
