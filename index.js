b9197d89b1493cbe2d6a46b088fe53e07dc2bb01
Gabriel BRUN <lydstyl@gmail.com>
2019-02-27 16:43:11 +0100

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+// génération de la ligne
+// <legacy URL> [i] p,[<protocol>],[<host>],<pipeline>[,<locale>][,<parameter name>,<parameter value>]*
+
+//     Search-ShowContent          /produits/pour-lui p,https,,Search-ShowContent,,fdid,Conseils-homme
+//     Search-Show                 /pour-elle/coiffure/seche-cheveux-pour-elle/  p,https,,Search-Show,,cgid,seche-cheveux
+//     Product-Show                /miroir-lumineux-rond-double-face  p,https,,Product-Show,,pid,8438E
+
+//     CustomerService-ContactUs   /assistance/  p,https,,CustomerService-ContactUs
+//     Home-Show                   /bandes-de-cire-froide-pour-jambes-corps-peaux-normales  p,https,,Home-Show
+//     Stores-Find                 /points-de-vente/ p,https,,Stores-Find
+
+
+// CustomerService-ContactUs
+// Home-Show
+// Product-Show
+// Search-Show
+// Search-ShowContent
+// Stores-Find
+
+
+
+
+// generation url de test
+// https://staging-na-conair.demandware.net/s/es-babyliss + /produits/pour-lui
+
+
+
